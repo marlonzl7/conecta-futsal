ad6477bc5ead80c1eac34e0fa9e92fdc6f3fda78
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-11-15 19:56:47 -0300

    feat(database): adicionado script para inserir categorias_padrão

---
+++ src/database/insert_categorias_padrao.sql
@@ -0,0 +1,21 @@
+USE conecta_futsal;
+
+INSERT INTO categoria_base (nome) VALUES
+    ('SUB-5'),
+    ('SUB-6'),
+    ('SUB-7'),
+    ('SUB-8'),
+    ('SUB-9'),
+    ('SUB-10'),
+    ('SUB-11'),
+    ('SUB-12'),
+    ('SUB-13'),
+    ('SUB-14'),
+    ('SUB-15'),
+    ('SUB-16'),
+    ('SUB-17'),
+    ('SUB-18'),
+    ('SUB-19'),
+    ('SUB-20'),
+    ('Profissional'),
+    ('Master');
