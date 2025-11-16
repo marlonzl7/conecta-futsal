7d108cdecf63f1119a7f3806fb5c80f112b89cf3
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-11-16 18:54:41 -0300

    chore(database): adicionado script para iniciar o banco, script para criar views e alterado endpoints para usar a view

---
+++ src/database/init-db.sql
@@ -0,0 +1,4 @@
+source ~/conecta_futsal/database/script.sql
+source ~/conecta_futsal/database/permissoes.sql
+source ~/conecta_futsal/database/dev_popular_banco.sql
+source ~/conecta_futsal/database/view.sql
