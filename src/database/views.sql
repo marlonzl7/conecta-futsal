51184a1954db2625a74fb9560cc799aa45e8077c
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-11-23 03:19:40 -0300

    feat(database): criado views que serão usadas para exibir gráficos na dashboard

--- src/database/views.sql
+++ src/database/views.sql
@@ -2,15 +2,34 @@ USE conecta_futsal;
 
 CREATE VIEW vw_listar_peneiras AS
     SELECT 
-            t.nome as time,
-            c.nome as 'categoria_de_base',
-            CONCAT(e.logradouro, ' ', e.numero, ' ', e.cidade) AS local,
-            p.data_hora_realizacao AS data
-        FROM 
-            peneira p 
-                JOIN endereco e 
-                    ON p.id_endereco = e.id_endereco 
-                JOIN time t 
-                    ON p.id_time = t.id_time
-                JOIN categoria_base c
-                    ON p.id_categoria_base = c.id_categoria_base;
+        t.nome as time,
+        c.nome as 'categoria_de_base',
+        CONCAT(e.logradouro, ' ', e.numero, ' ', e.cidade) AS local,
+        p.data_hora_realizacao AS data
+    FROM peneira p 
+    JOIN endereco e 
+        ON p.id_endereco = e.id_endereco 
+    JOIN time t 
+        ON p.id_time = t.id_time
+    JOIN categoria_base c
+        ON p.id_categoria_base = c.id_categoria_base;
+
+CREATE VIEW vw_listar_peneiras_por_cidade AS
+    SELECT
+        e.cidade,
+        COUNT(e.cidade) as quantidade
+    FROM peneira p
+    JOIN endereco e
+        ON p.id_endereco = e.id_endereco
+    GROUP BY
+        (e.cidade);
+
+CREATE VIEW vw_listar_peneiras_por_estado AS
+    SELECT
+        e.uf,
+        COUNT(e.uf) as quantidade
+    FROM peneira p
+    JOIN endereco e
+        ON p.id_endereco = e.id_endereco
+    GROUP BY
+        (e.uf);
