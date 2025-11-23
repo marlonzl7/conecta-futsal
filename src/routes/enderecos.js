63dfe76a17253c4a6abe53c43dd10720672c80ef
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-11-23 08:32:11 -0300

    refactor(endereco): corrigido rotas de endereços

--- src/routes/enderecos.js
+++ src/routes/enderecos.js
@@ -7,12 +7,16 @@ router.post("/cadastrar", function(req, res) {
     enderecoController.cadastrar(req, res);
 })
 
-router.get("/listar/idEndereco", function(req, res) {
-    enderecoController.listarPorIdEndereco(req, res);
+router.get("/listar/endereco/:idEndereco", function(req, res) {
+    enderecoController.buscarPorIdEndereco(req, res);
 })
 
-router.get("/listar/idUsuario", function(req, res) {
-    enderecoController.listarPorIdUsuario(req, res);
+router.get("/listar/usuario/:idUsuario", function(req, res) {
+    enderecoController.buscarPorIdUsuario(req, res);
+})
+
+router.get("/listar/ufs-validas", function(req, res) {
+    enderecoController.listarUFs(req, res);
 })
 
 module.exports = router;
