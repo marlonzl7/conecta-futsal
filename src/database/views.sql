fd4affb3f3419fd9380d03e32cfe0357f58fda34
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-11-22 21:11:20 -0300

    chore(database): alterado script para criação de views e popular banco

--- src/database/views.sql
+++ src/database/views.sql
@@ -2,10 +2,10 @@ USE conecta_futsal;
 
 CREATE VIEW vw_listar_peneiras AS
     SELECT 
-            t.nome as Time,
-            c.nome as 'Categoria de Base',
-            CONCAT(e.logradouro, ' ', e.numero, ' ', e.cidade) AS Local,
-            p.data_hora_realizacao AS Data
+            t.nome as time,
+            c.nome as 'categoria_de_base',
+            CONCAT(e.logradouro, ' ', e.numero, ' ', e.cidade) AS local,
+            p.data_hora_realizacao AS data
         FROM 
             peneira p 
                 JOIN endereco e 
