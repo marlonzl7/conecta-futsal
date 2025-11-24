760f558e499b7476a3c1abe4d591edb1a820af23
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-11-24 19:27:00 -0300

    chore(database): movido criação de views para o script de tabelas

--- src/database/init-db.sql
+++
@@ -1,4 +0,0 @@
-source /home/sptech/conecta_futsal/src/database/script.sql;
-source /home/sptech/conecta_futsal/src/database/permissoes.sql;
-source /home/sptech/conecta_futsal/src/database/dev_popular_banco.sql;
-source /home/sptech/conecta_futsal/src/database/views.sql;
--- src/database/script.sql
+++ src/database/script.sql
@@ -87,3 +87,39 @@ CREATE TABLE inscricao (
 	CONSTRAINT fk_inscricao_jogador FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador),
 	CONSTRAINT fk_inscricao_peneira FOREIGN KEY (id_peneira) REFERENCES peneira (id_peneira)
 );
+
+CREATE VIEW vw_listar_peneiras AS
+    SELECT 
+        p.id_peneira,
+        t.nome AS time,
+        c.nome AS categoria_de_base,
+        CONCAT(e.logradouro, ', ', e.numero, ' - ', e.cidade, ' ', e.uf) AS local,
+        p.data_hora_realizacao AS data
+    FROM peneira p 
+    JOIN endereco e 
+        ON p.id_endereco = e.id_endereco 
+    JOIN time t 
+        ON p.id_time = t.id_time
+    JOIN categoria_base c
+        ON p.id_categoria_base = c.id_categoria_base;
+
+CREATE VIEW vw_listar_peneiras_por_cidade AS
+    SELECT
+        CONCAT(e.cidade, ' - ', e.uf) AS cidade,
+        COUNT(*) AS quantidade
+    FROM peneira p
+    JOIN endereco e
+        ON p.id_endereco = e.id_endereco
+    GROUP BY
+        e.cidade,
+        e.uf;
+
+CREATE VIEW vw_listar_peneiras_por_estado AS
+    SELECT
+        e.uf,
+        COUNT(*) AS quantidade
+    FROM peneira p
+    JOIN endereco e
+        ON p.id_endereco = e.id_endereco
+    GROUP BY
+        e.uf;
